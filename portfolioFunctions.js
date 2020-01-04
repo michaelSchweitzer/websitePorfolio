696c833113c730b0665922da1bea344a41a43b30
Michael Schweitzer <maschweitzer87@gmail.com>
2020-01-04 13:26:00 -0600

    Added a button to scroll back to top of the page

---
+++ portfolioFunctions.js
@@ -0,0 +1,20 @@
+//To get icon
+var topIcon = document.getElementById("pageTop");
+
+//When user scrolls down 20px from the top of the page, the icon appears
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+        topIcon.style.display = "block";
+    }
+    else {
+        topIcon.style.display = "none";
+    }
+}
+
+//When user clicks icon, scroll to the top of the page
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
